dc610a8014253cd43374085ea4487a5962c731f5
Konstantin Bukin <bukin@inetstudio.ru>
2022-10-28 13:32:51 +0300

    Fixes

--- docker/php/aliases.sh
+++ docker/php/aliases.sh
@@ -1,2 +1,2 @@
-alias enable-xdebug="mv /usr/local/etc/php/conf.d/xdebug.ini.dis /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini && kill -USR2 1"
-alias disable-xdebug="mv /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini.dis && kill -USR2 1"
+alias enable-xdebug="mv /usr/local/etc/php/conf.d/xdebug.ini.dis /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini && kill -USR2 $(pgrep -P1 php-fpm)"
+alias disable-xdebug="mv /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini.dis && kill -USR2 $(pgrep -P1 php-fpm)"
